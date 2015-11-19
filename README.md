0798aea7999c3dc5558958289ddbdcbcaf341c7c
Dmitry Lazarev <diman.lazarev@gmail.com>
2015-11-19 10:08:22 +0300

    Adding free() for hostname

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ppingd
 Ping Daemon
+
+Use " gcc pping.c -o ppingd -lping" for build project.
--- pping.c
+++ pping.c
@@ -70,6 +70,7 @@ int main(int argc, char **argv)
 		}
 	}
 	main_loop();
+	free(hostname);
 	return 0;
 }
 
